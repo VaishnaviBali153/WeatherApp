1028e5d6cbb6d30352799a3a7f0ae270b78d6ad2
VaishnaviBali153 <95483554+VaishnaviBali153@users.noreply.github.com>
2021-12-24 19:25:21 +0530

    Add files via upload

---
+++ WeatherApp/WeatherApp/Weather.swift
@@ -0,0 +1,14 @@
+//
+//  Weather.swift
+//  WeatherApp
+//
+//  Created by DCS on 18/12/21.
+//  Copyright © 2021 DCS. All rights reserved.
+//
+
+import Foundation
+class Weather: Decodable {
+    var symbol = ""
+    var highPrice = ""
+    var lowPrice = ""
+}
